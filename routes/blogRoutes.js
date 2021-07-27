4dfa557e55a62b5e9a96be6d56e940446a032b43
Abantika Saha <65250567+abantika-saha@users.noreply.github.com>
2021-07-27 23:15:14 +0530

    Add files via upload

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -5,6 +5,8 @@ const router = express.Router();
 
 router.get('/create', blogController.blog_create_get);
 router.get('/', blogController.blog_index);
+router.get('/:id/edit', blogController.blog_edit_get);
+router.put('/:id', blogController.blog_edit_put);
 router.post('/', blogController.blog_create_post);
 router.get('/:id', blogController.blog_details);
 router.delete('/:id', blogController.blog_delete);
